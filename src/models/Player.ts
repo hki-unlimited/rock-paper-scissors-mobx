475bd1fcc36aeeb022c55d061357bbd4af051fab
Tähkävuori Vesa <vesa.tahkavuori@cinia.fi>
2019-09-21 16:06:53 +0300

    fine tuning

--- src/models/Player.ts
+++ src/models/Player.ts
@@ -1,13 +1,16 @@
 export interface IPlayer {
     id: number;
+    //name: string;
     isHuman: boolean;
 }
 
 export default class Player implements IPlayer {
     constructor (player: IPlayer) {
         this.id = player.id;
+        //this.name = player.name;
         this.isHuman = player.isHuman;
     }
     id: number;
+    //name: string;
     isHuman: boolean;
 }
